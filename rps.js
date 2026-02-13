d18b83aeb316e84608f7ac519a50e40be6ff5a4b
Janice <41716156+janice15@users.noreply.github.com>
2026-02-13 15:46:59 -0800

    Add score variables and function to play one round

--- rps.js
+++ rps.js
@@ -1,11 +1,11 @@
 const getComputerChoice = () => {
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
-        return "Rock";
+        return "ROCK";
     } else if (choice === 1) {
-        return "Paper";
+        return "PAPER";
     } else {
-        return "Scissors";
+        return "SCISSORS";
     }
 };
 
@@ -13,3 +13,30 @@ const getHumanChoice = () => {
     let humanInput = prompt("Choose: Rock, Paper, or Scissors!");
     return humanInput.toUpperCase();
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+const playRound = (humanChoice, computerChoice) => {
+  if (
+    (computerChoice === "ROCK" && humanChoice === "ROCK") ||
+    (computerChoice === "SCISSORS" && humanChoice === "SCISSORS") ||
+    (computerChoice === "PAPER" && humanChoice === "PAPER")
+  ){
+    console.log("It's a tie");
+  } else if (
+    (humanChoice === "ROCK" && computerChoice === "SCISSORS") ||
+    (humanChoice === "PAPER" && computerChoice === "ROCK") ||
+    (humanChoice === "SCISSORS" && computerChoice === "PAPER")
+  ) {
+    console.log(`${humanChoice} beats ${computerChoice}! You win!`);
+    humanScore += 1;
+  } else if (
+    (computerChoice === "ROCK" && humanChoice === "SCISSORS") ||
+    (computerChoice === "PAPER" && humanChoice === "ROCK") ||
+    (computerChoice === "SCISSORS" && humanChoice === "PAPER")
+  ) {
+    console.log(`${computerChoice} beats ${humanChoice}! Computer wins!`);
+    computerScore += 1;
+  }
+};
