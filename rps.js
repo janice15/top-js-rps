b47bb45a66bb23aef171bbf81da060c3cf8a85d7
Janice <41716156+janice15@users.noreply.github.com>
2026-02-25 14:19:38 -0800

    Add function to determine winner

--- rps.js
+++ rps.js
@@ -2,8 +2,6 @@ let playerScore = 0;
 let computerScore = 0;
 let currentRound = 0;
 
-
-
 const getComputerChoice = () => {
   let choice = Math.floor(Math.random() * 3);
   if (choice === 0) {
@@ -24,50 +22,36 @@ const getHumanChoice = () => {
 */
 
 const playRound = (humanChoice, computerChoice) => {
-    currentRound += 1
-    if (
-      (computerChoice === "ROCK" && humanChoice === "ROCK") ||
-      (computerChoice === "SCISSORS" && humanChoice === "SCISSORS") ||
-      (computerChoice === "PAPER" && humanChoice === "PAPER")
-    ) {
-      return ("It's a tie!");
-    } else if (
-      (humanChoice === "ROCK" && computerChoice === "SCISSORS") ||
-      (humanChoice === "PAPER" && computerChoice === "ROCK") ||
-      (humanChoice === "SCISSORS" && computerChoice === "PAPER")
-    ) {
-      playerScore += 1;
-      return (`${humanChoice} beats ${computerChoice}! You win!`);
-    } else if (
-      (computerChoice === "ROCK" && humanChoice === "SCISSORS") ||
-      (computerChoice === "PAPER" && humanChoice === "ROCK") ||
-      (computerChoice === "SCISSORS" && humanChoice === "PAPER")
-    ) {
-      computerScore += 1;
-      return (`${computerChoice} beats ${humanChoice}! Computer wins!`);
-      
-    }
-  };
-
-
-/*
-
-const playGame = () => {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-    console.log(playRound(getHumanChoice(), getComputerChoice()));
+  currentRound += 1;
+  if (
+    (computerChoice === "ROCK" && humanChoice === "ROCK") ||
+    (computerChoice === "SCISSORS" && humanChoice === "SCISSORS") ||
+    (computerChoice === "PAPER" && humanChoice === "PAPER")
+  ) {
+    return "It's a tie!";
+  } else if (
+    (humanChoice === "ROCK" && computerChoice === "SCISSORS") ||
+    (humanChoice === "PAPER" && computerChoice === "ROCK") ||
+    (humanChoice === "SCISSORS" && computerChoice === "PAPER")
+  ) {
+    playerScore += 1;
+    return `${humanChoice} beats ${computerChoice}! You win!`;
+  } else if (
+    (computerChoice === "ROCK" && humanChoice === "SCISSORS") ||
+    (computerChoice === "PAPER" && humanChoice === "ROCK") ||
+    (computerChoice === "SCISSORS" && humanChoice === "PAPER")
+  ) {
+    computerScore += 1;
+    return `${computerChoice} beats ${humanChoice}! Computer wins!`;
   }
-
-  if (humanScore > computerScore) {
-    console.log("You're the winner!");
-  } else if (computerScore > humanScore) {
-    console.log("Computer wins!");
-  } else {
-    console.log("It's a tie! No one wins!");
-  };
-  console.log(`Your score ${humanScore}. Computer score: ${computerScore}`);
 };
-*/
-//playGame();
+
+const getWinner = (playerScore, computerScore) => {
+  if (playerScore > computerScore) {
+    return "You're the winner! Congratulations!";
+  } else if (playerScore < computerScore) {
+    return "Computer wins! Better luck next time!";
+  } else {
+    return "It's a tie! No one wins!";
+  }
+};
